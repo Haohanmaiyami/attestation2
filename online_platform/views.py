a7c0f7ff504670793fd54d4b8622d3e8dcc7dc7d
Haohanmaiyami <kharitonovayan2018@gmail.com>
2025-10-30 22:47:34 -0400

    views.py

--- online_platform/views.py
+++ online_platform/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.permissions import IsAuthenticated
+
+from .models import Product, NetworkUnit
+from .serializers import ProductSerializer, NetworkUnitSerializer
+from .permissions import IsActiveStaff
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all().order_by('id')
+    serializer_class = ProductSerializer
+    permission_classes = [IsAuthenticated & IsActiveStaff]
+
+
+class NetworkUnitViewSet(viewsets.ModelViewSet):
+    """
+    CRUD для звена сети.
+    - `debt` редактировать через API нельзя (read_only).
+    - Фильтрация по стране: ?country=USA
+    """
+    queryset = NetworkUnit.objects.select_related('supplier').prefetch_related('products').order_by('id')
+    serializer_class = NetworkUnitSerializer
+    permission_classes = [IsAuthenticated & IsActiveStaff]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['country']
